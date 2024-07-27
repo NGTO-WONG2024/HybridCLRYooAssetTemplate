b10e8836f625699c8c1d84309bdab6f3acbeb40d
huangqiang <a454998235@gmail.com>
2024-07-27 12:19:54 +0800

    1

--- Assets/Script/Scripts_HotUpdate/CardBase.cs
+++ Assets/Script/Scripts_HotUpdate/CardBase.cs
@@ -14,7 +14,8 @@ namespace Script.Scripts_HotUpdate
     public class CardBase : MonoBehaviour
     {
         protected Dictionary<string, MMF_Player> mmfPlayers;
-        protected Dictionary<string, Image> images; 
+        protected Dictionary<string, Image> images;
+        protected Dictionary<string, AudioSource> audioSources;
         //protected Dictionary<string, Button> buttons;
         protected Dictionary<string, TextMeshProUGUI> texts;
         private MMFollowTarget followTarget;
@@ -25,6 +26,7 @@ namespace Script.Scripts_HotUpdate
             mmfPlayers = GetComponentsInChildren<MMF_Player>().ToDictionary(x => x.name, x => x);
             images = GetComponentsInChildren<Image>().ToDictionary(x => x.name, x => x);
             texts = GetComponentsInChildren<TextMeshProUGUI>().ToDictionary(x => x.name, x => x);
+            audioSources = GetComponentsInChildren<AudioSource>().ToDictionary(x => x.name, x => x);
             view = transform.GetChild(0);
             view.SetParent(Balatro.Instance.cardViewsParent);
             GetComponent<Button>().onClick.AddListener(OnClick);
@@ -33,13 +35,17 @@ namespace Script.Scripts_HotUpdate
 
         protected virtual void OnClick()
         {
-            
+            PlaySound("clickSound");
         }
         
         public async Task PlayFeelAsync(string feelName)
         {
             mmfPlayers[feelName].PlayFeedbacks();
             await Task.Delay((int)(mmfPlayers[feelName].TotalDuration * 1000));
+        }
+        public void PlaySound(string sound)
+        {
+            audioSources[sound].Play();
         }
     }
 }
